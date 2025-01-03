e74b5cde9e3453ac0d2a731270c01688e6c3acab
Fernando <101249579+fernand3z@users.noreply.github.com>
2025-01-03 21:23:03 +0000

    py3

--- feed.py
+++ feed.py
@@ -15,8 +15,26 @@ rss_element = xml_tree.Element('rss', {
 # Add the channel element
 channel_element = xml_tree.SubElement(rss_element, 'channel')
 
+link_prefix = yaml_data['link']
+
 # Add a title element within the channel
 xml_tree.SubElement(channel_element, 'title').text = yaml_data['title']
+xml_tree.SubElement(channel_element, 'subtitle').text = yaml_data['subtitle']
+xml_tree.SubElement(channel_element, 'format').text = yaml_data['format']
+xml_tree.SubElement(channel_element, 'itunes:author').text = yaml_data['author']
+xml_tree.SubElement(channel_element, 'itunes:image', {'href': link_prefix + yaml_data})
+xml_tree.SubElement(channel_element, 'language').text = yaml_data['language']
+xml_tree.SubElement(channel_element, 'link').text = link_prefix
+
+xml_tree.SubElement(channel_element, 'itunes:category', {'text': yaml_data['category']})
+
+for item in yaml_data[item]:
+    item_element = xml_tree.SubElement(channel_element, 'item')
+    xml_tree.SubElement(item_element, 'title').text= item['title']
+    xml_tree.SubElement(item_element, 'itunes:author').text= item['author']
+    xml_tree.SubElement(item_element, 'title').text= item['']
+    xml_tree.SubElement(item_element, 'title').text= item['title']
+    xml_tree.SubElement(item_element, 'title').text= item['title']
 
 # Write the XML tree to a file
 output_tree = xml_tree.ElementTree(rss_element)
